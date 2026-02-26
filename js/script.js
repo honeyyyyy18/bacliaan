cf40115f7e355c49f885ae07be1bf55f27b762c3
honeyyyyy18 <bacliaan.honeygrace@gmail.com>
2026-02-26 23:06:09 +0800

    fix submitBtn error

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 const themeToggle = document.getElementById("themeToggle");
-themeToggle.addEventListener("click", function(){
+if (themeToggle) {
+  themeToggle.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-    themeToggle.textContent = document.body.classList.contains("dark-mode") ? "Light Mode" : "Dark Mode" ;
-});
+    themeToggle.textContent =
+      document.body.classList.contains("dark-mode")
+        ? "Light Mode"
+        : "Dark Mode";
+  });
+}
 
 const toggleSkills = document.getElementById("toggleSkills");
 const skillsSection = document.getElementById("skillsSection");
-toggleSkills.addEventListener("click", function(){
+if (toggleSkills && skillsSection) {
+  toggleSkills.addEventListener("click", function () {
     skillsSection.classList.toggle("hidden");
-});
+  });
+}
 
-submitBtn.addEventListener("click", function(event){
+const submitBtn = document.getElementById("submitBtn");
+if (submitBtn) {
+  submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
     const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value; 
-    if (name==="" || email===""){
-        alert("Please fill in all required fields.")
+    const email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+      alert("Please fill in all required fields.");
     } else {
-        alert("Form submitted successfully!");
+      alert("Form submitted successfully!");
     }
-});
+  });
+}
